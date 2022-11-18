314d0a314358d45c3a99f747ebe1e83225db7314
Antony <ashout10@gmail.com>
2022-11-18 23:42:32 +0300

    if/else

---
+++ example5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Add User Name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "tony")
+{
+    Console.WriteLine("Congratulation!!! It's Tony!");
+}
+else
+{
+    Console.Write("Hello ");
+    Console.WriteLine(username);
+}
